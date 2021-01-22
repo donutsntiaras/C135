9b78ad0f4a379138435ccac239e188980081e78a
Donuts_n_Tiaras <61537524+donutsntiaras@users.noreply.github.com>
2021-01-22 15:25:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-video="";
+objects = [];
+status = "";
+video = "";
 
 function preload() {
     video = createVideo('video.mp4');
@@ -12,6 +14,30 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 480, 380);
+     if(status != ""){
+         objectDetector.detect(video, gotResult);
+         function draw() {
+            image(video, 0, 0, 380, 380); 
+        
+                  r=random(255);
+                  g=random(255);
+                  b=random(255);
+        
+                  objectDetector.detect(video, gotResult);
+        
+                  for (i=0;i<objects.length;i++) { 
+                  document.getElementById("status").innerHTML = "Status : Object Detected"; 
+                  document.getElementById("number_of_objects").innerHTML="Number of objects detected are : "+ objects.length;
+                  
+                  fill(r,g,b); 
+                  percent = floor(objects[i].confidence * 100); 
+                  text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15); 
+                  noFill(); 
+                  stroke(r,g,b); 
+                  rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height); 
+                }
+        }
+        }
 }
 
 function start() {
@@ -25,4 +51,11 @@ function modelLoaded() {
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error)
+    }
+    console.log(results);
 }
